258741a0b83e8bab5029344a0be855f6a500faca
Max Rösch <github@elbebass.com>
2022-01-28 14:26:47 +0100

    [TASK] Add php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,30 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->name('*.php')
+    ->in(__DIR__)
+    ->exclude('vendor');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony'               => true,
+        'binary_operator_spaces' => [
+            'operators' => [
+                '=>' => 'align',
+                '='  => 'align',
+            ],
+        ],
+        'phpdoc_to_comment' => false,
+        'ordered_imports'   => true,
+        'array_syntax'      => [
+            'syntax' => 'short',
+        ],
+        'no_unused_imports' => true,
+        /**
+         * Our templates rely heavily on things like endforeach, endif, etc.
+         * This setting should be turned off at least until we've switched to Twig
+         * (which is required for Symfony 5).
+         */
+        'no_alternative_syntax' => false,
+    ])
+    ->setFinder($finder);
